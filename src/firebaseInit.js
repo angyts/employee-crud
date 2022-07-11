7c5a1544131e044ab2816ce3bc6c9b7db17ee96c
Angyts <punggolzenith@gmail.com>
2022-07-11 11:24:31 +0800

    updated firestore credentials

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 // Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "AIzaSyC9J2hIQ1XQFx5NAD6pr37xBwNPQCTZZwY",
-  authDomain: "employee-crud-app-a3a32.firebaseapp.com",
-  databaseURL: "https://employee-crud-app-a3a32.firebaseio.com",
-  projectId: "employee-crud-app-a3a32",
-  storageBucket: "employee-crud-app-a3a32.appspot.com",
-  messagingSenderId: "87613015171",
-  appId: "1:87613015171:web:e60ee5139c56e5fc02c7b6"
+const firebaseConfig = {
+  apiKey: "AIzaSyBU8E77XlKgyXQSj_244jbqw1IjI4-lqDU",
+  authDomain: "diary-34c28.firebaseapp.com",
+  databaseURL: "https://diary-34c28.firebaseio.com",
+  projectId: "diary-34c28",
+  storageBucket: "diary-34c28.appspot.com",
+  messagingSenderId: "481441809494",
+  appId: "1:481441809494:web:55a7b8c9e396c90be8e7f7"
 };
 
 // Initialize Firebase
